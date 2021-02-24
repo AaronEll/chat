374b4d633cbcaaf700a2e1b6fb6c3331d1ff40e5
VoxiomC <79526508+VoxiomC@users.noreply.github.com>
2021-02-24 13:51:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ drone.on('error', error => {
 
 function getRandomName() {
   const adjs = ["Paper", "Egyptians", "Conspiracy", "Microchip", "Government", "Flat", "Shakespeare", "Dark", "Restraining", "Death", "Vaccine", "Corona", "Matter", "Black", "SOS", "Shrek", "Donkey", "Kyla", "&%*#", "Crimson"];
-  const nouns = ["Goblin", "BadManTouchMe", "AAAAAAAA", "HitByATrain", "Thunder", "GOD", "BloodSea", "SleepingDeath", "Paxton", "Genesis", "MustardGas", "Plans&Buildings", "Shadow", "Brain"];
+  const nouns = ["Goblin", "BadManTouchMe", "AAAAAAAA", "HitByATrain", "Thunder", "GOD", "BloodSea", "SleepingDeath", "Paxton", "Genesis", "MustardGas", "Planes&Buildings", "Shadow", "Brain"];
   return (
     adjs[Math.floor(Math.random() * adjs.length)] +
     "_" +
