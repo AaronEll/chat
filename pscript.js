a15045bc9dd6edfa907b8697db9b1920902cf876
VoxiomC <79526508+VoxiomC@users.noreply.github.com>
2021-03-05 13:40:33 -0500

    Update pscript.js

--- pscript.js
+++ pscript.js
@@ -57,8 +57,8 @@ drone.on('error', error => {
 });
 
 function getRandomName() {
-  const adjs = ["PacAnimal"];
-  const nouns = ["Liadon"];
+  const adjs = ["PacAnimal, Paxton"];
+  const nouns = ["Liadon, Clubb, 08"];
   return (
     adjs[Math.floor(Math.random() * adjs.length)] +
     "_" +
